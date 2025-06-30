5ae8e90893f91fc559b6801a269bebb1d7471872
rcitach <yearsince@yeah.net>
2025-06-30 16:26:54 +0800

    perfect rtt config (#15)

--- kernel/bsp/maix3/app_canmv/main.c
+++ kernel/bsp/maix3/app_canmv/main.c
@@ -159,7 +159,7 @@ static void check_bank_voltage(void)
   volatile rt_uint32_t read_result = *((rt_uint32_t *) virt_addr);
 
   if(0x00 == read_result) {
-#if !defined (CONFIG_BOARD_K230_CANMV) && !defined (CONFIG_BOARD_K230_CANMV_DONGSHANPI)
+#if !defined (CONFIG_BOARD_K230_CANMV) && !defined (CONFIG_BOARD_K230_CANMV_DONGSHANPI) && !defined (CONFIG_BOARD_K230_CANMV_RTT_EVB)
     rt_kprintf("\n\n\033[31mTHIS BOARD MAYBE NOT CONFIGURE BANK VOLTAGE!!!\n\n\033[0m");
 #endif
 
