d903f693caf4d60c30e295a8710e04469fe3a406
CLD <cld.username@gmail.com>
2025-11-14 15:26:24 +0800

    feat(usbh_serials): implement common USB serial devices.

---
+++ kernel/bsp/maix3/components/canmv_usb/usbh_serial_cp210x.c
@@ -0,0 +1,149 @@
+#include "dev_usbh_serial.h"
+#include "usbh_cp210x.h"
+#include "rtthread.h"
+
+#define DBG_SECTION_NAME    "USBH_SERIAL_CP210x"
+#define DBG_LEVEL           DBG_INFO
+#include <rtdbg.h>
+
+typedef struct usbh_cp210x_serial {
+    struct usbh_cp210x *cp210x;
+    usbh_serial_dev_t *dev;
+    rt_list_t list;
+} usbh_cp210x_serial_t;
+
+static rt_list_t s_serial_list = RT_LIST_OBJECT_INIT(s_serial_list);
+
+static char* get_dev_path(void *usbh_serial) {
+    struct usbh_cp210x *cp210x = usbh_serial;
+    return cp210x->hport->config.intf[cp210x->intf].devname;
+}
+
+static int set_line_coding(void *usbh_serial, struct cdc_line_coding *line_coding) {
+    struct usbh_cp210x *cp210x = usbh_serial;
+    LOG_I("%s set_line_coding", get_dev_path(usbh_serial));
+    LOG_I("dwDTERate = %d", line_coding->dwDTERate);
+    LOG_I("bCharFormat = %d", line_coding->bCharFormat);
+    LOG_I("bParityType = %d", line_coding->bParityType);
+    LOG_I("bDataBits = %d", line_coding->bDataBits);
+    return usbh_cp210x_set_line_coding(cp210x, line_coding);
+}
+
+static int get_line_coding(void *usbh_serial, struct cdc_line_coding *line_coding) {
+    struct usbh_cp210x *cp210x = usbh_serial;
+    LOG_D("%s get_line_coding", get_dev_path(usbh_serial));
+    return usbh_cp210x_get_line_coding(cp210x, line_coding);
+}
+
+static int set_line_state(void *usbh_serial, bool dtr, bool rts) {
+    struct usbh_cp210x *cp210x = usbh_serial;
+    LOG_D("%s set_line_state dtr=%d rts=%d", get_dev_path(usbh_serial), dtr, rts);
+    return usbh_cp210x_set_line_state(cp210x, dtr, rts);
+}
+
+static void set_bulk_in_transfer_buffer(void *usbh_serial, uint8_t *buffer, uint32_t buflen) {
+    struct usbh_cp210x *cp210x = usbh_serial;
+    LOG_D("%s set_bulk_in_transfer_buffer, buflen = %d", get_dev_path(usbh_serial), buflen);
+    usbh_bulk_urb_fill(&cp210x->bulkin_urb, cp210x->hport, cp210x->bulkin, buffer, buflen, 0, NULL, NULL);
+}
+
+static void set_bulk_out_transfer_buffer(void *usbh_serial, uint8_t *buffer, uint32_t buflen) {
+    struct usbh_cp210x *cp210x = usbh_serial;
+    LOG_D("%s set_bulk_out_transfer_buffer, buflen = %d", get_dev_path(usbh_serial), buflen);
+    usbh_bulk_urb_fill(&cp210x->bulkout_urb, cp210x->hport, cp210x->bulkout, buffer, buflen, 0, NULL, NULL);
+}
+
+static int bulk_in_transfer(void *usbh_serial, uint32_t timeout) {
+    struct usbh_cp210x *cp210x = usbh_serial;
+    LOG_D("%s bulk_in_transfer, timeout = %d", get_dev_path(usbh_serial), timeout);
+    cp210x->bulkin_urb.timeout = timeout;
+
+    return usbh_submit_urb(&cp210x->bulkin_urb);
+}
+
+static int bulk_out_transfer(void *usbh_serial, uint32_t timeout) {
+    struct usbh_cp210x *cp210x = usbh_serial;
+    LOG_D("%s bulk_out_transfer, timeout = %d", get_dev_path(usbh_serial), timeout);
+    cp210x->bulkout_urb.timeout = timeout;
+    return usbh_submit_urb(&cp210x->bulkout_urb);
+}
+
+static int async_bulk_in_transfer(void *usbh_serial, usbh_complete_callback_t complete, void *arg) {
+    struct usbh_cp210x *cp210x = usbh_serial;
+    LOG_D("%s async_bulk_in_transfer", get_dev_path(usbh_serial));
+    cp210x->bulkin_urb.timeout = 0;
+    cp210x->bulkin_urb.complete = complete;
+    cp210x->bulkin_urb.arg = arg;
+
+    return usbh_submit_urb(&cp210x->bulkin_urb);
+}
+
+static int async_bulk_out_transfer(void *usbh_serial, usbh_complete_callback_t complete, void *arg) {
+    struct usbh_cp210x *cp210x = usbh_serial;
+    LOG_D("%s async_bulk_out_transfer", get_dev_path(usbh_serial));
+    cp210x->bulkout_urb.timeout = 0;
+    cp210x->bulkout_urb.complete = complete;
+    cp210x->bulkout_urb.arg = arg;
+    return usbh_submit_urb(&cp210x->bulkout_urb);
+}
+
+static int cancel_bulk_in_transfer(void *usbh_serial) {
+    struct usbh_cp210x *cp210x = usbh_serial;
+    return usbh_kill_urb(&cp210x->bulkin_urb);
+}
+
+static int cancel_bulk_out_transfer(void *usbh_serial) {
+    struct usbh_cp210x *cp210x = usbh_serial;
+    return usbh_kill_urb(&cp210x->bulkout_urb);
+}
+
+static dev_usbh_serial_ops_t serial_ops = {
+    .get_dev_path = get_dev_path,
+    .set_line_coding = set_line_coding,
+    .get_line_coding = get_line_coding,
+    .set_line_state = set_line_state,
+    .set_bulk_in_transfer_buffer = set_bulk_in_transfer_buffer,
+    .set_bulk_out_transfer_buffer = set_bulk_out_transfer_buffer,
+    .bulk_in_transfer = bulk_in_transfer,
+    .bulk_out_transfer = bulk_out_transfer,
+    .async_bulk_in_transfer = async_bulk_in_transfer,
+    .async_bulk_out_transfer = async_bulk_out_transfer,
+    .cancel_bulk_in_transfer = cancel_bulk_in_transfer,
+    .cancel_bulk_out_transfer = cancel_bulk_out_transfer
+};
+
+void usbh_cp210x_run(struct usbh_cp210x *cp210x_class) {
+    LOG_I("usbh_cp210x_run!");
+    usbh_cp210x_serial_t *cp210x_serial = rt_malloc(sizeof(usbh_cp210x_serial_t));
+    if (cp210x_serial == RT_NULL) {
+        LOG_E("malloc usbh_cp210x_serial_t failed!");
+        return;
+    }
+
+    usbh_serial_dev_t *dev = dev_usbh_serial_create(cp210x_class, &serial_ops, 1024 * 8);
+    if (dev == RT_NULL) {
+        LOG_E("create usbh serial dev failed! name = %s", get_dev_path(cp210x_class));
+        return;
+    }
+
+    cp210x_serial->cp210x = cp210x_class;
+    cp210x_serial->dev = dev;
+    rt_list_init(&cp210x_serial->list);
+    rt_list_insert_after(&s_serial_list, &cp210x_serial->list);
+}
+
+void usbh_cp210x_stop(struct usbh_cp210x *cp210x_class) {
+    LOG_I("usbh_cp210x_stop!");
+    usbh_cp210x_serial_t *cp210x_serial;
+    rt_list_for_each_entry(cp210x_serial, &s_serial_list, list) {
+        if (cp210x_serial->cp210x = cp210x_class) {
+            break;
+        }
+    }
+
+    rt_list_remove(&cp210x_serial->list);
+
+    dev_usbh_serial_destroy(cp210x_serial->dev);
+
+    rt_free(cp210x_serial);
+}
